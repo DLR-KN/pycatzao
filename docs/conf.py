43f5b5755ba04a2c6bb0d29f4bff75f1ebbce3f8
Nis Meinert <nis.meinert@dlr.de>
2025-04-10 08:56:01 +0200

    Use https by default for Numpy documentation

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "numpy": ("http://docs.scipy.org/doc/numpy/", None),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
     "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
 }
 
